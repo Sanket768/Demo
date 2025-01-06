2f073297c8c24d7279c382fd40abaa9cae8c52c5
Sanket Dhakate <sanket.dhakate@pinnacle.in>
2025-01-06 15:18:30 +0530

    Delete 27122024 directory

--- 27122024/Streams/Java.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.*;  
-public class JavaStream3 {  
-    public static void main(String[] args){  
-        Stream.iterate(1, element->element+1)  
-        .filter(element->element%5==0)  
-        .limit(5)  
-        .forEach(System.out::println);  
-    }  
-}  
